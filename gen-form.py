831b2325c7c69642cd663594ea9b92cdaf713842
hoytnix <>
2025-02-16 13:53:17 -0500

    Last Sync: 2025-02-16 18:53:17 (Mobile)

--- gen-form.py
+++ gen-form.py
@@ -10,6 +10,7 @@ def generate_form():
     hpxml_root = parse_xsd('HPXML.xsd')
     data_types_root = parse_xsd('DataTypes.xsd')
     base_elements_root = parse_xsd('BaseElements.xsd')
+    merged_root = parse_xsd('HPXMLMerged.xsd')
     
     # Extract namespaces
     ns = {'xs': 'http://www.w3.org/2001/XMLSchema'}
@@ -43,41 +44,40 @@ def generate_form():
             <div class="mb-3">
                 <form class="needs-validation" novalidate>'''
 
-        # Find inputs for this element from the base elements
+        # Find complexType definition for this element
         type_name = elem.tag.split('}')[1]  # Get the tag name without the namespace
         if type_name:
-            # Find complexType definition
-            #type_def = base_elements_root.find(f".//xs:complexType[@name='{type_name}']", ns)
-            for type_def in base_elements_root.findall(f".//xs:element[@name='{type_name}']", ns):
-                if type_def is not None:
-                    # Find the nested elements within the complexType
-                    base_elements = type_def.findall(".//xs:element", ns)
-                    for base_elem in base_elements:
-                        # Discover the type of the element
-                        data_type = base_elem.get('type', '')
-                        if data_type:
-                            type_def = data_types_root.find(f".//xs:simpleType[@name='{data_type}']", ns)
-                            if type_def is not None:
-                                input_type = "text"  # default
-                                if "Date" in data_type:
-                                    input_type = "date"
-                                elif "Boolean" in data_type:
-                                    input_type = "checkbox"
-                                elif "Number" in data_type or "Integer" in data_type:
-                                    input_type = "number"
-                                elif "String" in data_type:
-                                    input_type = "text"
-                                # Add more checks for other data types here
+            # Find complexType definition in merged schema
+            type_def = merged_root.find(f".//xs:complexType[@name='{type_name}']", ns)
+            if type_def is not None:
+                # Find the nested elements within the complexType
+                base_elements = type_def.findall(".//xs:element", ns)
+                for base_elem in base_elements:
+                    # Discover the type of the element
+                    data_type = base_elem.get('type', '')
+                    if data_type:
+                        type_def = data_types_root.find(f".//xs:simpleType[@name='{data_type}']", ns)
+                        if type_def is not None:
+                            input_type = "text"  # default
+                            if "Date" in data_type:
+                                input_type = "date"
+                            elif "Boolean" in data_type:
+                                input_type = "checkbox"
+                            elif "Number" in data_type or "Integer" in data_type:
+                                input_type = "number"
+                            elif "String" in data_type:
+                                input_type = "text"
+                            # Add more checks for other data types here
 
-                                panels_html += f'''
-                                <div class="mb-3">
-                                    <label for="{base_elem.get('name')}" class="form-label">{base_elem.get('name')}</label>
-                                    <input type="{input_type}" class="form-control" 
-                                           id="{base_elem.get('name')}" name="{base_elem.get('name')}" required>
-                                    <div class="invalid-feedback">
-                                        Please provide a valid {base_elem.get('name')}.
-                                    </div>
-                                </div>'''
+                            panels_html += f'''
+                            <div class="mb-3">
+                                <label for="{base_elem.get('name')}" class="form-label">{base_elem.get('name')}</label>
+                                <input type="{input_type}" class="form-control" 
+                                       id="{base_elem.get('name')}" name="{base_elem.get('name')}" required>
+                                <div class="invalid-feedback">
+                                    Please provide a valid {base_elem.get('name')}.
+                                </div>
+                            </div>'''
 
         panels_html += '''
                     <button type="submit" class="btn btn-primary">Submit</button>

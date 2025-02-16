e1502f08fdf39f452df22b9c18f96899a8cd4565
Michael Hoyt <hoytnix@users.noreply.github.com>
2025-02-16 11:22:16 -0500

    Create jinja-gen.py

---
+++ jinja-gen.py
@@ -0,0 +1,19 @@
+from jinja2 import Template
+
+# Read the template file
+with open('index.html', 'r') as file:
+    template_content = file.read()
+
+# Read the form HTML content
+with open('hpxml_form.html', 'r') as file:
+    form_html = file.read()
+
+# Create Jinja template
+template = Template(template_content)
+
+# Render template with form HTML
+output_html = template.render(form_html=form_html)
+
+# Write the result to a new file
+with open('output.html', 'w+') as file:
+    file.write(output_html)

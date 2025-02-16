cd831729a30e8818972970e823d1976d6147dfc2
hoytnix <>
2025-02-16 17:23:38 -0500

    Last Sync: 2025-02-16 22:23:38 (Mobile)

--- gen-form.py
+++ gen-form.py
@@ -8,7 +8,7 @@ def parse_xsd(filepath):
 
 def generate_form():
     # Parse the XSD files
-    hpxml_root = parse_xsd('HPXMLMerged.xsd')
+    hpxml_root = parse_xsd('HPXML.xsd')
     data_types_root = parse_xsd('DataTypes.xsd')
     base_elements_root = parse_xsd('BaseElements.xsd')
     
@@ -43,14 +43,54 @@ def generate_form():
              id="panel-{name}" role="tabpanel" tabindex="0">
             <div class="mb-3">
                 <form class="needs-validation" novalidate>'''
-        
+
         # Find complexType definition for this element
         type_name = elem.tag.split('}')[1]  # Get the tag name without the namespace
         if type_name:
-            # Find the complexType definition in base_elements_root
+            # Find complexType definition in hpxml_root
+            # Find the nested elements within the complexType
+            for type_def in hpxml_root.findall(f".//xs:complexType[@name='{type_name}']", ns):
+                panels_html += process_complexType(type_def, ns, data_types_root)
+
+        panels_html += '''
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>'''
+    
+    # Process elements from BaseElements.xsd
+    for i, elem in enumerate(tqdm(base_elements_root.findall(".//xs:element", ns), desc="Generating Form")):
+        name = elem.get('name', '')
+        if not name:
+            continue
+            
+        # Generate tab
+        active = 'active' if i == 0 else ''
+        tab_id = f"tab-{name}"
+        
+        tabs_html += f'''
+        <li class="nav-item" role="presentation">
+            <button class="nav-link {active}" id="{tab_id}" data-bs-toggle="tab" 
+                    data-bs-target="#panel-{name}" type="button" role="tab">
+                {name}
+            </button>
+        </li>'''
+        
+        # Generate panel content
+        panels_html += f'''
+        <div class="tab-pane fade {'show ' if i == 0 else ''}{active}" 
+             id="panel-{name}" role="tabpanel" tabindex="0">
+            <div class="mb-3">
+                <form class="needs-validation" novalidate>'''
+
+        # Find complexType definition for this element
+        type_name = elem.tag.split('}')[1]  # Get the tag name without the namespace
+        if type_name:
+            # Find complexType definition in hpxml_root
+            # Find the nested elements within the complexType
             for type_def in base_elements_root.findall(f".//xs:complexType[@name='{type_name}']", ns):
                 panels_html += process_complexType(type_def, ns, data_types_root)
-                
+
         panels_html += '''
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
@@ -87,7 +127,8 @@ def process_complexType(type_def, ns, data_types_root):
                     input_type = "number"
                 elif "String" in data_type:
                     input_type = "text"
-                
+                # Add more checks for other data types here
+
                 html += f'''
                 <div class="mb-3">
                     <label for="{base_elem.get('name')}" class="form-label">{base_elem.get('name')}</label>
@@ -97,9 +138,8 @@ def process_complexType(type_def, ns, data_types_root):
                         Please provide a valid {base_elem.get('name')}.
                     </div>
                 </div>'''
-        
         # Recursively process nested complexTypes
-        for nested_type_def in base_elem.findall(".//xs:complexType", ns):
+        for nested_type_def in base_elem.findall(f".//xs:complexType", ns):
             html += process_complexType(nested_type_def, ns, data_types_root)
     return html
 
